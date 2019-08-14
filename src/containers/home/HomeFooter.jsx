146d09fbcb954034e47117b17e39c2b101a13a76
YudhaDwipriatma <yudha_dwipriatma_26rpl@student.smktelkom-mlg.sch.id>
2019-08-14 17:39:13 +0700

    footer

---
+++ src/containers/home/HomeFooter.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const HomeFooter = () => {
+    return (
+        <div>
+             <div className="footer-top">
+            <div className="col-lg-5 col-md-6 col-sm-6">
+              
+            </div>
+            <div className="col-lg-2 col-md-6 col-sm-6 social-widget">
+              <div className="single-footer-widget">
+                <div className="no-socia ld-flex align-items-center">
+                <a href="#">
+                <img className="telepon" src="../../public/img/home/Group 103.png" alt="" />
+                <img className="footer-img-3"  src="../../public/img/home/08123434488.png" alt="" />
+              </a>
+              <a href="#">
+                <img className="email" src="../../public/img/home/Group 102.png" alt=""/>
+                <img className="footer-img-2" src="../../public/img/home/andree@twisode.com.png" alt=""/>
+              </a>
+              <a href="#">
+                <img className="location" src="../../public/img/home/Group 101.png" alt="" />
+                <img className="footer-img-1" src="../../public/img/home/Scomptec Building Jl. Kayun no.24 (R. 303 - 304) Surabaya.png" alt=""/>
+              </a>
+    
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+};
+
+export default HomeFooter;
--- src/containers/home/HomeTeam.jsx
+++ src/containers/home/HomeTeam.jsx
@@ -43,13 +43,12 @@ export default class Responsive extends Component {
 
       <section className="">
       <div className="container-fluid">
-<<<<<<< HEAD
+
       <div className="wrapper-team">
         <h2 className="text-center"> MEET OUR TEAM. </h2>
-=======
+
       <div className="slider">
         <h2 className="team text-center no-span"> MEET OUR TEAM. </h2>
->>>>>>> master
         <p className="text-muted text-center">We have teams of great developers ready to help you grow your business</p>
 
     
@@ -224,6 +223,7 @@ export default class Responsive extends Component {
          
    
         </Slider>
+      </div>
       </div>
         </div>
           </section>
